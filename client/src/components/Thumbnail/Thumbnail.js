bfb435e61ab7a42722c4e53585f3418153a034a7
Kamy <kamy.leeret@gmail.com>
2020-06-27 20:32:41 -0600

    add thumbnail

---
+++ client/src/components/Thumbnail/Thumbnail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// This file exports both the List and ListItem components
+
+function Thumbnail(props) {
+    return (
+        <div role="img">
+            <img className="border border-info" alt={props.title} src={props.image}/>
+        </div>
+    )
+
+export default Thumbnail;
--- client/src/pages/AddBooks.js
+++ client/src/pages/AddBooks.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import List from "../components/List/List";
 import API from "../utils/API";
 import { Input, FormBtn } from "../components/Forms/AddBook"
+import Thumbnail from "../components/Thumbnail/Thumbnail'";
 
 class Search extends Component {
   state = {
@@ -65,6 +66,44 @@ class Search extends Component {
               </FormBtn>
             </form>
       </div>
+      <div>
+      {this.state.searchBooks.length ? (
+        <List>
+          {this.state.searchBooks.map(book => (
+            <Row className="search-row" key={book.id}>
+              <Col size="md-3">
+                <Thumbnail
+                  title={book.title}
+                  image={book.image}
+                />
+                <strong>
+                  {book.title} by {book.author}
+                </strong>
+              </Col>
+              <Col size="md-7">
+                {book.synopsis}
+              </Col>
+              <Col size="md-2">
+                <SaveBtn onClick={() => this.saveBook(
+                  book.id,
+                  book.title,
+                  book.author,
+                  book.synopsis,
+                  book.image,
+                  book.link)
+                } />
+                <LinkBtn onClick={() => this.goToLink(book.link)}/>
+              </Col>
+            </Row>
+          ))}
+        </List>
+      ) : (
+          <h3>Enter Title and Author to Search</h3>
+        )}
+    </Col>
+
+    </div>
+
     );
   }
 }
